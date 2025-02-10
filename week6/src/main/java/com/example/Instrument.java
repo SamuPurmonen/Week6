8dba014128ff107d5cecee957ece8562d39e0f66
SamuPurmonen <samu.purmonen@student.lut.fi>
2025-02-10 18:02:08 +0200

    dasdsad

--- week6/src/main/java/com/example/Instrument.java
+++ week6/src/main/java/com/example/Instrument.java
@@ -29,9 +29,11 @@ public class Instrument {
 
             for (int i = 0; i < Instruments.size(); i++) {
                 System.out.println("Valmistaja: " + Instruments.get(i).manufacturer + ", Hinta: " + Instruments.get(i).price + "e");
+                
             }
         }
-        return "Valmistaja: " + Instruments.get(i).manufacturer + ", Hinta: " + Instruments.get(i).price + "e";
+        
+        return "Valmistaja: " + Instruments.get(Instruments.size()).manufacturer + ", Hinta: " + Instruments.get(Instruments.size()).price + "e";
         // miksi tässä pitäisi edes palauttaa mitään kun se voi kertoa ne yksityiskohdat tässä
     }
 
